ffbd83d2f6b1fe54e62e16e8246c0e1df82ae39e
reneemic <reneemic75@gmail.com>
2019-04-14 09:41:30 -0400

    login class

--- src/main/java/org/wecancodeit/medicinecabinetapp/base/classes/Login.java
+++ src/main/java/org/wecancodeit/medicinecabinetapp/base/classes/Login.java
@@ -1,31 +1,23 @@
 package org.wecancodeit.medicinecabinetapp.base.classes;
 
-
-import java.sql.SQLException;
-
-
-@SuppressWarnings("unused")
 public class Login {
-	
-	private String username;
-	  private String password;
-	
-	  
-	  public String getUsername() {
-	  return username;
-	  }
-	  
-	  public void setUsername(String username) {
-	  this.username = username;
-	  }
-	  
-	  public String getPassword() {
-	  return password;
-	  }
-	  
-	  public void setPassword(String password) {
-	  this.password = password;
-	  }
-	 
-	  }
-	  
+
+	private String userName;
+	private String userPassword;
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getUserPassword() {
+		return userPassword;
+	}
+
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
+}
