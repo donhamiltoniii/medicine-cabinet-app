3f38925112507afb9b76c1319a247ec53e190627
reneemic <reneemic75@gmail.com>
2019-04-14 09:07:27 -0400

    registration controller

--- src/main/java/org/wecancodeit/medicinecabinetapp/controllers/RegistrationController.java
+++ src/main/java/org/wecancodeit/medicinecabinetapp/controllers/RegistrationController.java
@@ -1,37 +1,19 @@
 package org.wecancodeit.medicinecabinetapp.controllers;
 
-
-
 import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetailsService;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.ModelAndView;
-import org.wecancodeit.medicinecabinetapp.base.classes.User;
-import org.wecancodeit.medicinecabinetapp.dataaccess.UserService;
-import org.wecancodeit.medicinecabinetapp.controllers.RegistrationRepository;
 
 @Controller
 public class RegistrationController {
-
-@Resource
-RegistrationRepository registrationrepo;
-
-
 	
+	@Resource
+	RegistrationRepository registrationRepo; 
+
+	@RequestMapping("/register")
+	public String registerPage(Model model) {
+		return "registerPage";
+	}
 }
-
-
-
-
-
-	
-	
-
