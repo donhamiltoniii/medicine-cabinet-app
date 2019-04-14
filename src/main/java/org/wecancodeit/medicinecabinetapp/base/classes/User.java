06f7254029a61088f7539c0b07debb3031565ccf
reneemic <reneemic75@gmail.com>
2019-04-14 09:21:32 -0400

    user class

--- src/main/java/org/wecancodeit/medicinecabinetapp/base/classes/User.java
+++ src/main/java/org/wecancodeit/medicinecabinetapp/base/classes/User.java
@@ -1,16 +1,20 @@
 package org.wecancodeit.medicinecabinetapp.base.classes;
 
+import java.util.Set;
 
-import javax.annotation.Generated;
 import javax.persistence.Entity;
+
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
 
+import org.springframework.data.annotation.Transient;
 
 @Entity
+@Table(name="user")
 public class User {
-
-
+	
 	@Id
 	@GeneratedValue
 	private long id;
@@ -22,19 +26,26 @@ public class User {
 	private String userPhone;
 	private String userEmail;
 
+	
+@Transient
+private String passwordConfirm;
 
-	public User (String userName, String firstName, String lastName,String userPassword, String userPhone, String userEmail) {
-
-		this.userName=userName;
-		this.firstName = firstName;
+@ManyToMany
+private Set<Role> roles;
+	
+	public User () {
+		
+	}
+	
+	public User(String userName, String firstName, String lastName, String userPassword, String userPhone, String userEmail) {
+		this.userName = userName;
+		this.firstName=firstName;
 		this.lastName=lastName;
 		this.userPassword = userPassword;
 		this.userPhone = userPhone;
 		this.userEmail=userEmail;
-
-
-}
-
+	}
+	
 	public long getId() {
 		return id;
 	}
@@ -43,34 +54,33 @@ public class User {
 		return userName;
 	}
 
-	 public void setUserName(String userName) {
-		  this.userName = userName;
-		  }
-	 
+	public void setUserName(String userName) {
+		this.userName=userName;
+	}
+	
 	public String getFirstName() {
 		return firstName;
 	}
 	
-	 public void setFirstName(String firstName) {
-		  this.firstName = firstName;
-		  }
-	 
-	 public String getLastName() {
-			return lastName;
-		}
-		
-	public void setLastName(String lastName) {
-	       this.lastName = lastName;
+	public void setFirstName(String firstName) {
+		this.firstName=firstName;
 	}
-		
-
+	
+	public String lastName() {
+		return lastName;
+	}
+	public void setLastname(String lastName) {
+		this.lastName=lastName;
+	}
+	
 	public String getUserPassword() {
 		return userPassword;
 	}
+	
 	public void setUserPassword(String userPassword) {
-		  this.userPassword = userPassword;
-    }
-
+		this.userPassword=userPassword; 
+	}
+	
 	public String getUserPhone() {
 		return userPhone;
 	}
@@ -82,13 +92,27 @@ public class User {
 	public String getUserEmail() {
 		return userEmail;
 	}
-
+	
 	public void setUserEmail(String userEmail) {
 		this.userEmail=userEmail;
 	}
-
-
 	
+	public String getPasswordConfirm() {
+		return passwordConfirm;
+	}
+	
+	public void setPasswordConfirm(String passwordConfirm) {
+		this.passwordConfirm=passwordConfirm;
+	}
+	
+	public Set<Role> getRoles(){
+		return roles;
+	}
+	
+	public void setRoles(Set<Role> roles) {
+		this.roles=roles;
+	}
+
 }
 
 
